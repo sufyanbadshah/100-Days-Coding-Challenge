bcf58ba91a6e49766bb046548c1ac88dd28fc574
Sufyan Badshah <sufyanbadshah898@gmail.com>
2024-03-16 19:45:29 +0500

    Add files via upload

---
+++ Day1/main.js
@@ -0,0 +1,6 @@
+// let Name = "Sufyan"
+// console.log(`Hey ${Name} would you like to learn typescript today?`)
+var Name = "sUfYaN";
+console.log(Name.toLowerCase());
+console.log(Name.toUpperCase());
+console.log(Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase());
---
+++ Day1/main.ts
@@ -0,0 +1,8 @@
+ let Name = "Sufyan"
+console.log(`Hey ${Name} would you like to learn typescript today?`)
+
+
+let Name = "sUfYaN"
+console.log(Name.toLowerCase())
+console.log(Name.toUpperCase())
+console.log(Name.charAt(0).toUpperCase()+Name.slice(1).toLowerCase())
