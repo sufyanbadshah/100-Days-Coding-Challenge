0c922226742e6d341b56480f423b86fae0f9eb8c
Sufyan Badshah <sufyanbadshah898@gmail.com>
2024-03-16 19:42:27 +0500

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-// let Name = "Sufyan"
-// console.log(`Hey ${Name} would you like to learn typescript today?`)
-var Name = "sUfYaN";
-console.log(Name.toLowerCase());
-console.log(Name.toUpperCase());
-console.log(Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase());
